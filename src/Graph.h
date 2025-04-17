7197a000b5e0492007482419697b4ed8c2b18ad0
vincinious <87542036+vincinious@users.noreply.github.com>
2025-04-17 02:52:19 -0700

    Add Graph.h (WIP)

---
+++ src/Graph.h
@@ -0,0 +1,32 @@
+
+/* Completed by Vinicius Intravartola */
+#include <string>
+#include <list>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+class Graph 
+{
+   private:
+
+        /* adjacency list to map a vertex(string) to a list of vertices(neighbors). */
+        unordered_map <string, vector <string> > adjList;
+
+    public:
+
+        Graph();
+
+        /* Add a vertex to the graph if it does not already exist */
+        void addVertex(const string& vertex);
+
+        /* Add an edge, undirected (directs to both edges) or directed */
+        void addEdge(const string& from, const string& to, bool undirected = true);
+
+        void printGraph() const;
+
+        //~Graph();
+
+};
+
